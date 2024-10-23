791e3c1f2214d1b31d734bf35ba70b42a52a986d
Antoine Delubriac <Antoine.DELUBRIAC@stagiaire-elan.fr>
2024-10-23 11:58:37 +0200

    Méthode + vue Module

--- src/Controller/ModuleController.php
+++ src/Controller/ModuleController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller;
 
+use App\Repository\ModuleRepository;
+use App\Repository\SessionModuleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ModuleController extends AbstractController
 {
-    #[Route('/module', name: 'app_module')]
-    public function index(): Response
+    #[Route('module', name: 'list_module')]
+    public function list(ModuleRepository $moduleRepository, SessionModuleRepository $sessionModuleRepository): Response
     {
-        return $this->render('module/index.html.twig', [
-            'controller_name' => 'ModuleController',
+        $modules = $moduleRepository->findAll();
+        $sessionsModules = $sessionModuleRepository->findAll();
+
+        return $this->render('module/list.html.twig', [
+            'modules' => $modules,
+            'sessionsModules' => $sessionsModules,
         ]);
     }
 }
