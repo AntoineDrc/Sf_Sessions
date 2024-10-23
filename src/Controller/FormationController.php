50a38528243a8641e8927300cc0c7cb6e40ab840
Antoine Delubriac <Antoine.DELUBRIAC@stagiaire-elan.fr>
2024-10-23 11:43:23 +0200

    Méthode + vue Formations

--- src/Controller/FormationController.php
+++ src/Controller/FormationController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller;
 
+use App\Repository\FormationRepository;
+use App\Repository\SessionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class FormationController extends AbstractController
 {
-    #[Route('/formation', name: 'app_formation')]
-    public function index(): Response
+    #[Route('formation', name: 'list_formation')]
+    public function list(FormationRepository $formationRepository, SessionRepository $sessionRepository): Response
     {
-        return $this->render('formation/index.html.twig', [
-            'controller_name' => 'FormationController',
+        $formations = $formationRepository->findAll();
+        $sessions = $sessionRepository->findAll();
+
+        // Calcul le taux de remplissage en %
+        foreach ($sessions as $session)
+        {
+            $session->tauxRemplissage = ($session->getInterns()->count() / $session->getCapacity()) * 100;
+        }
+
+        return $this->render('formation/list.html.twig', [
+            'formations' => $formations,
+            'sessions' => $sessions,
         ]);
     }
 }
