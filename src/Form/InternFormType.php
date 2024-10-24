672826f1802c6b63b44c2195115bfd720208d473
Antoine Delubriac <Antoine.DELUBRIAC@stagiaire-elan.fr>
2024-10-24 13:51:58 +0200

    Ajouts CSS + formulaire Intern

---
+++ src/Form/InternFormType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Intern;
+use App\Entity\Session;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class InternFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstName')
+            ->add('lastName')
+            ->add('birthDate', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('city')
+            ->add('email')
+            ->add('phone')
+            ->add('sex')
+            ->add('sessions', EntityType::class, [
+                'class' => Session::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Intern::class,
+        ]);
+    }
+}
