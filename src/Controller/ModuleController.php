0b2621f4e98ef653e3e40a6f4d9f11c9fbf61764
Antoine Delubriac <Antoine.DELUBRIAC@stagiaire-elan.fr>
2024-10-24 19:28:01 +0200

    Vue + formulaire + méthode (en cours) 'new_module'

--- src/Controller/ModuleController.php
+++ src/Controller/ModuleController.php
@@ -2,23 +2,56 @@
 
 namespace App\Controller;
 
+use App\Entity\Module;
+use App\Entity\SessionModule;
+use App\Form\ModuleFormType;
 use App\Repository\ModuleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\SessionModuleRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ModuleController extends AbstractController
 {
     #[Route('module', name: 'list_module')]
-    public function list(ModuleRepository $moduleRepository, SessionModuleRepository $sessionModuleRepository): Response
+    public function list(SessionModuleRepository $sessionModuleRepository): Response
     {
-        $modules = $moduleRepository->findAll();
         $sessionsModules = $sessionModuleRepository->findAll();
 
         return $this->render('module/list.html.twig', [
-            'modules' => $modules,
             'sessionsModules' => $sessionsModules,
+        ]);
+    }
+
+    // Méthode d'ajout de module
+    #[Route('module/new', name: 'new_module')]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $module = new Module();
+
+       // Crée le formulaire sur le modele de la classe Intern généré par la console
+       $form = $this->createForm(ModuleFormType::class, $module);
+
+       // Gère la requete pour remplir le formulaire avec les données soumises
+       $form->handleRequest($request);
+
+       if ($form->isSubmitted() && $form->isValid())
+       {
+           // Persiste les données de la classe Intern dans la BDD
+           $entityManager->persist($module);
+
+           // Sauvegarde les changements en BDD
+           $entityManager->flush();
+
+           // Redirige apres soumission
+           return $this->redirectToRoute('list_module');
+       }
+
+        // Si le formulaire n'a pas encore été soumis, affiche le formulaire
+        return $this->render('module/new.html.twig', [
+            'formAddModule' => $form,
         ]);
     }
 }
