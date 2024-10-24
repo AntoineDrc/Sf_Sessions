ebed64a8d76b4ce7c36d208b31863c771167d79c
Antoine Delubriac <Antoine.DELUBRIAC@stagiaire-elan.fr>
2024-10-24 20:23:22 +0200

    Vues + méthodes + Form (Module, Session)

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -3,6 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Session;
+use App\Form\SessionFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,6 +18,37 @@ class SessionController extends AbstractController
 
         return $this->render('session/detail.html.twig', [
             'session' => $session,
+        ]);
+    }
+
+    
+    // Méthode d'ajout d'une session
+    #[Route('session/new', name: 'new_session')]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $intern = new Session();
+
+        // Crée le formulaire sur le modele de la classe Intern généré par la console
+        $form = $this->createForm(SessionFormType::class, $intern);
+
+        // Gère la requete pour remplir le formulaire avec les données soumises
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            // Persiste les données de la classe Intern dans la BDD
+            $entityManager->persist($intern);
+
+            // Sauvegarde les changements en BDD
+            $entityManager->flush();
+
+            // Redirige apres soumission
+            return $this->redirectToRoute('listById_formation');
+        }
+
+        // Si le formulaire n'a pas encore été soumis, affiche le formulaire
+        return $this->render('session/new.html.twig', [
+            'formAddSession' => $form,
         ]);
     }
 
---
+++ src/Form/SessionFormType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Formation;
+use App\Entity\Intern;
+use App\Entity\Session;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SessionFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('capacity', NumberType::class)
+            ->add('startDate', DateTimeType::class, [
+                'widget' => 'single_text',
+            ])
+            ->add('endDate', DateTimeType::class, [
+                'widget' => 'single_text',
+            ])
+            ->add('interns', EntityType::class, [
+                'class' => Intern::class,
+                'choice_label' => 'lastName',
+                'multiple' => true,
+            ])
+            ->add('formation', EntityType::class, [
+                'class' => Formation::class,
+                'choice_label' => 'name',
+            ])
+            ->add('valider', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Session::class,
+        ]);
+    }
+}
